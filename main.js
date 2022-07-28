8c1b829f1c48d94701488e056baf28d79ac0b3a1
liadk07 <91607695+liad07@users.noreply.github.com>
2022-07-29 00:20:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//https://www.ehna.tv/
+//https://best.aliexpress.com/
+if (window.location.origin=="https://www.ehna.tv"||window.location.origin=="https://best.aliexpress.com"||window.location.origin=="https://join.worldoftanks.eu"||window.location.origin=="https://www.arab48.com"||window.location.origin=="https://freshaudio.net"||window.location.origin=="https://snpossible.com"||window.location.origin=="https://adfpoint.com"||window.location.origin=="https://blockall.net"){
+    window.close()
+}
+else {
+    console.log(1)
+}
